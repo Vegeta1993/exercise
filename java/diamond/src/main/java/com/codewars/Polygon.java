c45695f1347fbcd03ef6a6c831a7bdea879c062f
Avinash Halsnad <halsna01@ads.uni-passau.de>
2019-09-28 12:35:38 +0200

    Polygon size problem solved

--- java/diamond/src/main/java/com/codewars/Polygon.java
+++ java/diamond/src/main/java/com/codewars/Polygon.java
@@ -1,5 +1,15 @@
 package com.codewars;
 
+import java.text.DecimalFormat;
+
 public class Polygon {
 
+  private static DecimalFormat df2 = new DecimalFormat("##.###");
+
+  public static double areaOfPolygonInsideCircle(double circleRadius, int numberOfSides) {
+    double sinof = 360.000 / numberOfSides;
+    double sinpart = Math.sin(Math.toRadians(sinof));
+    double otherpart = numberOfSides * Math.pow(circleRadius, 2) * 1 / 2;
+    return Double.parseDouble(df2.format(otherpart * sinpart));
+  }
 }
--- java/diamond/src/test/java/com/codewars/PolygonTest.java
+++ java/diamond/src/test/java/com/codewars/PolygonTest.java
@@ -1,5 +1,28 @@
 package com.codewars;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class PolygonTest {
 
+  @Test
+  public void test1() {
+    assertEquals(11.691, Polygon.areaOfPolygonInsideCircle(3, 3), 1e-4);
+  }
+
+  @Test
+  public void test2() {
+    assertEquals(8, Polygon.areaOfPolygonInsideCircle(2, 4), 1e-4);
+  }
+
+  @Test
+  public void test3() {
+    assertEquals(14.86, Polygon.areaOfPolygonInsideCircle(2.5, 5), 1e-4);
+  }
+
+  @Test
+  public void test4() {
+    assertEquals(27.335, Polygon.areaOfPolygonInsideCircle(3, 14), 1e-4);
+  }
 }
